cb1a4d29a6ff9bf16bea4184dc7d9be744df0e09
Vladimir <137224235+Teftelion@users.noreply.github.com>
2024-12-04 12:12:44 +0500

    Update Product.vue

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,1 +1,25 @@
+<template>
+  <div>
+    <h1>{{ product.name }}</h1>
+    <p>{{ product.description }}</p>
+    <p>Price: ${{ product.price }}</p>
+    <button @click="addToCart(product)">Add to Cart</button>
+  </div>
+</template>
+
+<script>
+import { products } from '../mock/products.js';
+import { useRoute } from 'vue-router';
+
+export default {
+  setup() {
+    const route = useRoute();
+    const product = products.find((p) => p.id === Number(route.params.id));
+    const addToCart = (product) => {
+      console.log('Added to cart:', product);
+    };
+    return { product, addToCart };
+  },
+};
+</script>
 
