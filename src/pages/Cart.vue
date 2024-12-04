888a7f69529ecdd282a23402fae0781e0288da52
Vladimir <137224235+Teftelion@users.noreply.github.com>
2024-12-04 12:12:54 +0500

    Update Cart.vue

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,1 +1,21 @@
+<template>
+  <div>
+    <h1>Cart</h1>
+    <div v-if="cart.length === 0">Your cart is empty.</div>
+    <ul v-else>
+      <li v-for="item in cart" :key="item.id">{{ item.name }} - ${{ item.price }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const cart = ref([]);
+    return { cart };
+  },
+};
+</script>
 
