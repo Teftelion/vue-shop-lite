27ab8f34b3554c64b1bdea8b4c741e284d80154a
Vladimir <137224235+Teftelion@users.noreply.github.com>
2024-12-04 12:12:20 +0500

    Update ProductList.vue

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,1 +1,28 @@
+<template>
+  <div class="product-list">
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+      @add-to-cart="addToCart"
+    />
+  </div>
+</template>
+
+<script>
+import ProductCard from './ProductCard.vue';
+import { products } from '../mock/products.js';
+
+export default {
+  components: { ProductCard },
+  setup() {
+    const cart = [];
+    const addToCart = (product) => {
+      cart.push(product);
+      console.log('Cart:', cart);
+    };
+    return { products, addToCart };
+  },
+};
+</script>
 
