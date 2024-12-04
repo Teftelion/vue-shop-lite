895bf5711543763b2d17dd1f9f35478a831917df
Vladimir <137224235+Teftelion@users.noreply.github.com>
2024-12-04 12:12:10 +0500

    Update ProductCard.vue

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,1 +1,15 @@
+<template>
+  <div class="product-card">
+    <h3>{{ product.name }}</h3>
+    <p>Price: ${{ product.price }}</p>
+    <button @click="$emit('add-to-cart', product)">Add to Cart</button>
+    <router-link :to="`/product/${product.id}`">View Details</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+};
+</script>
 
