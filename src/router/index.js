12310d034f1d140adc1ec0cf480c1bab395a3cbe
Vladimir <137224235+Teftelion@users.noreply.github.com>
2024-12-04 12:11:30 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+import Product from '../pages/Product.vue';
+import Cart from '../pages/Cart.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/product/:id', name: 'Product', component: Product },
+  { path: '/cart', name: 'Cart', component: Cart },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
 
